2bb8aa9dbd75afc742cff9c3c09a56965c4ba59f
Nikolai Akimov <gaporf@mail.ru>
2021-10-23 08:15:12 +0000

    networkfs: add lab
    Co-Authored-By: Nikita Sychev <root@nsychev.ru>

---
+++ entrypoint.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ivanov Ivan");
+MODULE_VERSION("0.01");
+
+char buffer[8192];
+
+int networkfs_init(void) {
+  printk(KERN_INFO "Hello, World!\n");
+  return 0;
+}
+
+void networkfs_exit(void) { printk(KERN_INFO "Goodbye!\n"); }
+
+module_init(networkfs_init);
+module_exit(networkfs_exit);
